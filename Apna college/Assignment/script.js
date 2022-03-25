363362df46251834be1699986764639cabfeeb39
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-25 15:46:14 +0530

    form js completed

--- Apna college/Assignment/script.js
+++ Apna college/Assignment/script.js
@@ -1,43 +1,58 @@
 
-const names=document.getElementById('name');
-const pass=document.getElementById('pass');
-const form=document.getElementById('form')
-const output=document.getElementById('output')
+const names = document.getElementById('name');
+const pass = document.getElementById('pass');
+const form = document.getElementById('form')
+const error = document.getElementById('error')
+const output = document.getElementById('Output')
 
 function getSentence() {
-    return document.getElementById('name').value;   
-  }
+    return document.getElementById('name').value;
+}
 function getChar() {
-    return document.getElementById('pass').value;   
-  }
+    return document.getElementById('pass').value;
+}
 
 
-form.addEventListener('submit',(e)=>{
-    // let text=e.getElementById('name').innerText;
-    // let Char=e.getElementById('pass').innerText;
-    
-    let outputValue=[]; 
-    let sentence=getSentence();
-    let char=getChar();
+form.addEventListener('submit', (e) => {
 
-    outputValue.push(sentence);
-    outputValue.push(char);
-    
-    if(names.value === ''){  
-       outputValue.push("Sentence is required")
+    let outputValue = [];
+    let outputString = [];
+
+    if (names.value === '') {
+        outputValue.push("Sentence is required")
     }
-    if(pass.value.length<1){
+    if (pass.value.length < 1) {
         outputValue.push("Input must be 1 Char")
     }
-    
-
-    if(outputValue.length>0){
-        e.preventDefault();
-        output.innerText=outputValue.join(',')
-        console.log(sentence);
+    if (pass.value.length > 1) {
+        outputValue.push("Input must be 1 Char")
     }
-    // else{
-    //     sentence.innerText=(char)
-    // }
-    
+
+    if (outputValue.length > 0) {
+        e.preventDefault();
+        error.innerText = outputValue.join(',')
+
+    }
+    else {
+        error.innerText=("");
+        let sentence = getSentence();
+        let char = getChar();
+
+        let idx = sentence.indexOf(char);
+
+        if (idx==-1) {
+            e.preventDefault();
+            output.innerText = ("The letter does not exists in the sentence")
+        }
+        else {
+            let s = sentence.slice(idx + 1);
+
+            outputString.push(s);
+
+            e.preventDefault();
+
+            output.innerText = outputString;
+        }
+    }
+
 })
