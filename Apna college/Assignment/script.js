e6c1babd57d884e7295d9bd9a815e8ac2e58975c
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-03-25 16:49:46 +0530

    page completed

--- Apna college/Assignment/script.js
+++ Apna college/Assignment/script.js
@@ -1,15 +1,15 @@
 
-const names = document.getElementById('name');
-const pass = document.getElementById('pass');
+const names = document.getElementById('sentence');
+const pass = document.getElementById('letter');
 const form = document.getElementById('form')
-const error = document.getElementById('error')
+
 const output = document.getElementById('Output')
 
 function getSentence() {
-    return document.getElementById('name').value;
+    return document.getElementById('sentence').value;
 }
 function getChar() {
-    return document.getElementById('pass').value;
+    return document.getElementById('letter').value;
 }
 
 
@@ -19,22 +19,22 @@ form.addEventListener('submit', (e) => {
     let outputString = [];
 
     if (names.value === '') {
-        outputValue.push("Sentence is required")
+        outputValue.push("Sentence cannot be empty ")
     }
     if (pass.value.length < 1) {
-        outputValue.push("Input must be 1 Char")
+        outputValue.push("letter cannot be empty ")
     }
     if (pass.value.length > 1) {
-        outputValue.push("Input must be 1 Char")
+        outputValue.push("letter must be 1 Char")
     }
 
     if (outputValue.length > 0) {
         e.preventDefault();
-        error.innerText = outputValue.join(',')
+        output.innerText = outputValue.join(', ')
 
     }
     else {
-        error.innerText=("");
+        
         let sentence = getSentence();
         let char = getChar();
 
@@ -46,11 +46,8 @@ form.addEventListener('submit', (e) => {
         }
         else {
             let s = sentence.slice(idx + 1);
-
             outputString.push(s);
-
             e.preventDefault();
-
             output.innerText = outputString;
         }
     }
